f5f7c366972d1c5631cb42fac269227970516cd4
antlord <antlord92@gmail.com>
2014-06-21 03:51:29 +0900

    Ogre string methods changed by STL methods

--- src/CustomTerrainGenerator.cpp
+++ src/CustomTerrainGenerator.cpp
@@ -1,15 +1,18 @@
 #include "../include/CustomTerrainGenerator.h"
 #include <iostream>
+#include <string.h>
+#include <sstream>
 void CustomTerrainGenerator::define(TerrainGroup* terrainGroup, long x, long y)
 {
     const int MAX_VALUE = 1;
     Image img;
-    Ogre::String name = "nsmall-";
+    std::stringstream qwe;
     int c = (x+y*4);
-    name += Ogre::StringConverter::toString(c)+".png";
-    std::cout<<"\nx: "<<x<<"\ny: "<<y<<std::endl;
-    std::cout<<"\nname: "<<name.c_str()<<std::endl;
-    img.load(name, ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+    qwe<<"nsmall-"<<c<<".png";
+    //name += Ogre::StringConverter::toString(c)+".png";
+    //std::cout<<"\nx: "<<x<<"\ny: "<<y<<std::endl;
+    //std::cout<<"\nname: "<<name.c_str()<<std::endl;
+    img.load(qwe.str(), ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
     //img.load("heightmap.png", ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
     //if (x % 2)
         //img.flipAroundY();
