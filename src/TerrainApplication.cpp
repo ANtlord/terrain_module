32f41b75a99349ef0b0de072270eebd70703b215
antlord <antlord92@gmail.com>
2014-07-01 17:00:25 +0900

    hex values of size of world changed by decimal

--- src/TerrainApplication.cpp
+++ src/TerrainApplication.cpp
@@ -16,10 +16,15 @@ This source file is part of the
 */
 #include "../include/TerrainApplication.h"
 #include "../include/CustomProfile.h"
-#define ENDLESS_PAGE_MIN_X (0x0)
-#define ENDLESS_PAGE_MIN_Y (0x0)
-#define ENDLESS_PAGE_MAX_X 0x3
-#define ENDLESS_PAGE_MAX_Y 0x3
+//#define ENDLESS_PAGE_MIN_X (0x0)
+//#define ENDLESS_PAGE_MIN_Y (0x0)
+//#define ENDLESS_PAGE_MAX_X 0x3
+//#define ENDLESS_PAGE_MAX_Y 0x3
+
+#define ENDLESS_PAGE_MIN_X 0
+#define ENDLESS_PAGE_MIN_Y 0
+#define ENDLESS_PAGE_MAX_X 3
+#define ENDLESS_PAGE_MAX_Y 3
 
 #define TERRAIN_WORLD_SIZE 6000.0f
 #define TERRAIN_SIZE 513
